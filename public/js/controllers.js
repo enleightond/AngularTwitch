b36cd68e420f8cd477f7369b94624cfe945767e0
enleightond <enleightond@yahoo.com>
2016-03-23 15:12:36 -0700

    popular games and streamers populating in dashboard. search query for games also populating, but not for streamers

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,40 +8,39 @@ app.controller('LoginController', ['$scope','$location','searchService', functio
 }]);
 
 app.controller('SignupController', ['$scope','$location','searchService','$http', function($scope, $location, searchService, $http){
-		// $scope.user = {};
-		$scope.error = '';		
-		$scope.submit=function(){
-			$http({
-				method: 'POST',
-				url:'/api/signup',
-				data: {user:$scope.user}
-			});
-		}		
+	$scope.error = '';		
+	$scope.submit=function(){
+		$http({
+			method: 'POST',
+			url:'/api/signup',
+			data: {user:$scope.user}
+		});
+	}		
 }])
 
 app.controller('DashboardController', ['$scope','$location','searchService', '$http', function($scope, $location, searchService, $http) {
 	var topGames = 'https://api.twitch.tv/kraken/games/top?limit=5'
 	var topStreamers = 'https://api.twitch.tv/kraken/streams?limit=10'
-	console.log(topGames, topStreamers)
 	// get me stuff once when the pages loads
 	$http({
 		method: 'GET',
 		url: topGames
 	}).then(function successCallback(res){
-		$scope.data.top[].game.medium;
-	}, function failedAtEverythingCallback(res){
-		console.log("THE SYSTEM IS DOWN", res);
+		$scope.popGames = res.data.top;//.game.box.medium;
+	}, function failCallback(res){
+		console.log("THE SYSTEM IS DOWN");
 	});
 
 	$http({
 		method: 'GET',
 		url: topStreamers
 	}).then(function successCallback(res){
-		$scope.data.streams[].preview.small;
-	}, function failedAtEverythingCallback(res){
+		console.log(res)
+		$scope.popStreamers = res.data.streams;//.preview.small;
+		$scope.popViewers = res.data.streams;//.viewers;
+	}, function failCallback(res){
 		console.log("THE SYSTEM IS DOWN", res);
 	});
-
 	// get me stuff once a search is called
 	$scope.searchTwitch = function () {		
 		var searchString =
@@ -53,42 +52,12 @@ app.controller('DashboardController', ['$scope','$location','searchService', '$h
 				method: 'GET',
 				url: searchString
 			}).then(function successCallback(res){
-				console.log(res);
-			}, function failedAtEverythingCallback(res){
+				$scope.searchQuery = res.data.games;//.box.medium;
+				$scope.searchStreamers = 				
+			}, function failedCallback(res){
 				console.log("THE SYSTEM IS DOWN", res);
 			})
-		// $http(get me some shit at this address) {
-		// 	when you got it,  log it out to me.
-		// }
 	}
-
-
-
-			
-		// searchService.getGames().then(function(data) {
-		// 	$scope.games = data('/dashboard/search', function(req,res, next) {
-
-		// 		request.get(twitchUrl, function (error, response, body) {
-		// 		  	var data = JSON.parse(body);
-
-		// 			request.get(twitchUrl2, function (error, response, body) {
-		// 			  	data.streamData = JSON.parse(body);
-
-		// 				request.get(twitchUrl3, function (error, response, body) {
-		// 				  	data.streamerData = JSON.parse(body);
-
-		// 					request.get(twitchUrl4, function (error, response, body) {
-		// 					  	data.gameImg = JSON.parse(body);
-
-
-		// 						res.render('dashboard', {games: data.games, streams: data.streamData.streams, streams: data.streamerData.streams, games: data.gameImg.games } );
-
-		// 					});
-		// 				});
-		// 			});
-		// 		});
-		// 	});
-		// });
 }])
 
 
